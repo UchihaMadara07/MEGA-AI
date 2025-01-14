9df14e74c95edf445ab8bea7fbb53798e52c5ec9
MADARA <145380330+UchihaMadara07@users.noreply.github.com>
2025-01-15 00:37:25 +0530

    Update Alive.js

--- plugins/Alive.js
+++ plugins/Alive.js
@@ -17,14 +17,14 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
   };
 
   let messageContent = {
-    text: '𝗨𝗟𝗧𝗥𝗔-𝗠𝗗 𝗜𝗦 𝗥𝗨𝗡𝗡𝗜𝗡𝗚', // Text content in case a message body is needed
+    text: '𝗢𝗕𝗜𝗧𝗢-𝗕𝗢𝗧𝗭𝗭 𝗜𝗦 𝗥𝗨𝗡𝗡𝗜𝗡𝗚', // Text content in case a message body is needed
     contextInfo: {
       mentionedJid: [m.sender],
       externalAdReply: {
-        title: '© 𝙶𝚕𝚘𝚋𝚊𝚕𝚃𝚎𝚌𝚑𝙸𝚗𝚏𝚘',
-        body: '© GlobalTechInfo',
+        title: '© 𝗢𝗕𝗜𝗧𝗢-𝗕𝗢𝗧𝗭𝗭',
+        body: '© 𝗢𝗕𝗜𝗧𝗢-𝗕𝗢𝗧𝗭𝗭',
         thumbnailUrl: img,
-        sourceUrl: 'https://whatsapp.com/channel/0029VagJIAr3bbVBCpEkAM07',
+        sourceUrl: 'https://chat.whatsapp.com/KWAadDw545X2Re1F6RjjEz',
         mediaType: 1,
         renderLargerThumbnail: true,
       },
