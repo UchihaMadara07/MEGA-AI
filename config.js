4867d345b30af8c11a09d18a9623e99bdaced7ef
MADARA <145380330+UchihaMadara07@users.noreply.github.com>
2025-01-15 00:34:11 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ import fs from 'fs'
 import dotenv from 'dotenv'
 dotenv.config()
 
-const defaultOwner = '923444844060';
+const defaultOwner = '918101821810';
 
 
 // Check for the OWNERS environment variable; if not found, use the default
@@ -19,13 +19,13 @@ for (let i = 0; i < ownerlist.length; i++) {
     global.owner.push([ownerlist[i], true]);
 }
 //
-global.botname = process.env.BOTNAME || 'MEGA-AI';
+global.botname = process.env.BOTNAME || '𝗢𝗕𝗜𝗧𝗢-𝗕𝗢𝗧𝗭𝗭';
 global.pairingNumber = process.env.BOT_NUMBER || '' ;  // put your number here
 global.SESSION_ID = process.env.SESSION_ID || '' ;  // put your session id here
 
 global.mods = []
 global.prems = []
-global.allowed = ['923444844060', '923051391007']
+global.allowed = ['918101821810', '855316492719']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
 global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
 global.keysxteammm = [
@@ -42,7 +42,7 @@ global.keysneoxrrr = ['5VC9rvNx', 'cfALv5']
 global.keysneoxr = keysneoxrrr[Math.floor(keysneoxrrr.length * Math.random())]
 global.lolkeysapi = ['GataDios']
 
-global.canal = 'https://whatsapp.com/channel/0029VagJIAr3bbVBCpEkAM07'
+global.canal = 'https://chat.whatsapp.com/KWAadDw545X2Re1F6RjjEz'
 
 
 global.APIs = {
@@ -73,15 +73,15 @@ global.APIKeys = {
 
 // Sticker WM
 global.premium = 'true'
-global.packname = 'MEGA-AI'
-global.author = 'GlobalTechInfo'
-global.menuvid = 'https://i.imgur.com/2U2K9YA.mp4'
-global.igfg = ' Follow on Instagram\nhttps://www.instagram.com/global.techinfo'
-global.dygp = 'https://whatsapp.com/channel/0029VagJIAr3bbVBCpEkAM07'
-global.fgsc = 'https://github.com/GlobalTechInfo/MEGA-AI'
-global.fgyt = 'https://youtube.com/@GlobalTechInfo'
-global.fgpyp = 'https://youtube.com/@GlobalTechInfo'
-global.fglog = 'https://i.ibb.co/G2dh9cB/qasim.jpg'
+global.packname = '★ σвιтσ-кυη ★'
+global.author = '🇮🇳🍁'
+global.menuvid = 'https://i.imgur.com/lROEr5r.mp4'
+global.igfg = ' Follow on Instagram\nhttps://instagram.com/_.obito_uchiha_786'
+global.dygp = 'https://chat.whatsapp.com/KWAadDw545X2Re1F6RjjEz'
+global.fgsc = 'https://github.com/obitoxd/OBITO-BOTZZ'
+global.fgyt = 'https://youtube.com/@kamui_edits'
+global.fgpyp = 'https://youtube.com/@kamui_edits'
+global.fglog = 'https://i.imgur.com/y7cGHfe.jpg'
 global.thumb = fs.readFileSync('./assets/A.jpg')
 
 global.wait = '⏳'
