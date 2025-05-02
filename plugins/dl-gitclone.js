a384ff106c40e29f0556abcd8d7e83ee12bc1840
MADARA <145380330+UchihaMadara07@users.noreply.github.com>
2025-05-02 21:10:32 +0530

    Update dl-gitclone.js

--- plugins/dl-gitclone.js
+++ plugins/dl-gitclone.js
@@ -2,7 +2,7 @@ import fetch from 'node-fetch'
 const regex = /(?:https|git)(?::\/\/|@)github\.com[\/:]([^\/:]+)\/(.+)/i
 let handler = async (m, { conn, args, usedPrefix, command }) => {
   if (!args[0])
-    throw `Where is the github link?\n\n📌 Example : ${usedPrefix + command} https://github.com/GlobalTechInfo`
+    throw `Where is the github link?\n\n📌 Example : ${usedPrefix + command} https://github.com/obitoxd/OBITO-BOTZZ`
   if (!regex.test(args[0])) throw '⚠️ link incorrect'
   let [_, user, repo] = args[0].match(regex) || []
   repo = repo.replace(/.git$/, '')
